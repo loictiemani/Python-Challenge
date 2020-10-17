bfeccde6e0d7a50fec0316aa82361dc8a7db2844
Loic Tiemani <loictiems@github.com>
2020-10-16 22:38:23 -0400

    Pushing updates

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -9,8 +9,9 @@ election_path = os.path.join(".","PyPoll" ,"Resources","election_data.csv")
 
 # Lists to store data/ Variables
 total_votes = 0
-total_votesc = 0
-Candidates_with_votes = []
+Candidates = {}
+percentage={}
+AllValues=[]
 
 # Open and read the CSV
 with open(election_path, "r") as file:
@@ -23,22 +24,43 @@ with open(election_path, "r") as file:
     
     for row in csv_reader:
        
-       total_votes+=1
+        total_votes+=1
        
-       if row[2] not in Candidates_with_votes:
+        if row[2] not in Candidates.keys():
         
-           Candidates_with_votes.append(row[2])
-        
-       if row[2] == Candidates_with_votes[0]:
-
-            total_votesc+=1   
-       percentage = total_votesc/total_votes
+            Candidates[row[2]]=1
+        else:
+            Candidates[row[2]]+=1
+    percentage = dict(Candidates)
 
 
-print(f"Election Results")
-print("--------------------------")
-print(f"Total Votes: {total_votes}")  
-print("--------------------------")   
-print(f"{Candidates_with_votes}") 
-print(f"Total Votes: {total_votesc}") 
-print(f"{percentage}")
+Poll_analysis = os.path.join(".", "PyPoll","Analysis","Poll_analysis.txt") 
+
+with open(Poll_analysis, "w") as txtfile:
+
+# Write Data to text file
+    
+    txtfile.write(f"Election Results\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+
+    print(f"Election Results")
+    print("--------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("--------------------------")
+    txtfile.write("--------------------------\n")
+    for k,v in  percentage.items(): 
+        percentage[k] =  round(v *100/ total_votes,2)
+        print(f"{k}: {percentage[k]}% ({v})")
+        AllValues.append(percentage[k])
+        txtfile.write(f"{k}: {percentage[k]}% ({v})\n")
+    print("--------------------------")
+    txtfile.write("--------------------------\n")
+    maximum_value = max(AllValues)
+
+    for k,v in  percentage.items():
+        if v==maximum_value :
+            print(f"Winner: {k}")
+            txtfile.write(f"Winner: {k}\n")
+    print("--------------------------")
+    txtfile.write("--------------------------")
